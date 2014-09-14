b3950619f610f7f1ea9b7387660f9bc47f00483e
MadArkitekt <edwardcsalter@gmail.com>
2014-09-13 20:17:17 -0400

    GIF added

--- DigitalPond/DPARipple.m
+++ DigitalPond/DPARipple.m
@@ -7,34 +7,32 @@
 //
 
 #import "DPARipple.h"
+@interface DPARipple()
+@end
 
 @implementation DPARipple
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
     }
     return self;
 }
 
--(void)didMoveToWindow
-{
-    for (int i = 0; i < self.rippleCount; i++)
-    {
-        float delayAmount = (self.rippleLifeTime / self.rippleCount) * i;
+- (void)didMoveToWindow {
+    for (int i = 0; i < self.rippleCount; i++) {
+        float delayAmount = (self.rippleLifeTime / self.rippleCount) * 3*i;
         [self rippleLineWithDelay:delayAmount];
     }
 }
 
--(void)rippleLineWithDelay:(float)delay
-{
+- (void)rippleLineWithDelay:(float)delay {
     UIView  *rippleLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
     rippleLine.backgroundColor = self.tintColor;
     [self addSubview:rippleLine];
     [UIView animateWithDuration:self.rippleLifeTime delay:delay options:UIViewAnimationOptionCurveEaseOut animations:^{
-        rippleLine.frame = CGRectMake(-40, -40, 80, 80);
-//      rippleLine.layer.cornerRadius = 40;
+        rippleLine.frame = CGRectMake(-40, -40, 60, 60);
+
         rippleLine.alpha = 0;
     } completion:^(BOOL finished) {
         [rippleLine removeFromSuperview];
